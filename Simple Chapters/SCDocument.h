78a9680f9942d6fe2fd7aec3f0c61b61cca7aaeb
schultyy <janschulte90@googlemail.com>
2012-04-08 20:24:43 +0200

    Initial Commit

---
+++ Simple Chapters/SCDocument.h
@@ -0,0 +1,13 @@
+//
+//  SCDocument.h
+//  Simple Chapters
+//
+//  Created by Jan Schulte on 08.04.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface SCDocument : NSDocument
+
+@end
---
+++ Simple Chapters/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Simple Chapters
+//
+//  Created by Jan Schulte on 08.04.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
