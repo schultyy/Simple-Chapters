253c681ea92d1a444c779b0729e0651812b62687
schultyy <janschulte90@googlemail.com>
2012-04-10 22:17:09 +0200

    Added FeedDetailSheet

---
+++ Simple Chapters/FeedDetailSheet.h
@@ -0,0 +1,13 @@
+//
+//  FeedDetailSheet.h
+//  Simple Chapters
+//
+//  Created by Jan Schulte on 10.04.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface FeedDetailSheet : NSWindowController
+
+@end
---
+++ Simple Chapters/FeedDetailSheet.m
@@ -0,0 +1,14 @@
+//
+//  FeedDetailSheet.m
+//  Simple Chapters
+//
+//  Created by Jan Schulte on 10.04.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "FeedDetailSheet.h"
+
+@implementation FeedDetailSheet
+
+
+@end
