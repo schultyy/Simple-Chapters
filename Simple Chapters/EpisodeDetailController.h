27ed950d588e21aed218e9889cc8d9c9e4aa2910
schultyy <janschulte90@googlemail.com>
2012-07-16 20:09:54 +0200

    Created EpisodeDetailSheet.
    EpisodeDetailSheet shows episode details and chapter informations.

---
+++ Simple Chapters/EpisodeDetailController.h
@@ -0,0 +1,13 @@
+//
+//  EpisodeDetailController.h
+//  Simple Chapters
+//
+//  Created by Jan Schulte on 16.07.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface EpisodeDetailController : NSViewController
+
+@end
---
+++ Simple Chapters/EpisodeDetailController.m
@@ -0,0 +1,23 @@
+//
+//  EpisodeDetailController.m
+//  Simple Chapters
+//
+//  Created by Jan Schulte on 16.07.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "EpisodeDetailController.h"
+
+@implementation EpisodeDetailController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
